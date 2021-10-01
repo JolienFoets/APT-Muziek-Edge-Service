dc7fa8ff217b997ba39d9a0db9e3ba3eb37ae88d
oomsBritt <r0802207@student.thomasmore.be>
2021-10-01 10:57:19 +0200

    init

---
+++ src/test/java/com/example/musicedgeservice/MusicedgeserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicedgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicedgeserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
