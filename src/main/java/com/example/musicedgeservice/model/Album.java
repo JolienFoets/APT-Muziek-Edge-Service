ecca1398cc0ae3908b585589811c99d9527444ae
Ooms Britt <57892631+Britt93@users.noreply.github.com>
2021-12-10 10:31:35 +0100

    album model aangevuld

--- src/main/java/com/example/musicedgeservice/model/Album.java
+++ src/main/java/com/example/musicedgeservice/model/Album.java
@@ -1,5 +1,49 @@
 package com.example.musicedgeservice.model;
 
 public class Album {
-    //Nog aanvullen met info van BRITT
+    private int id;
+
+    private int albumId;
+
+    private String mbid;
+    private String title;
+/*
+    public Album() {
+    }
+
+    public Album(int id, int albumId, String mbid, String title) {
+        this.id = id;
+        this.albumId = albumId;
+        this.mbid = mbid;
+        this.title = title;
+    }
+*/
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAlbumId() { return albumId; }
+
+    public void setAlbumId(int albumId) { this.albumId = albumId; }
+
+    public String getMbid() {
+        return mbid;
+    }
+
+    public void setMbid(String mbid) {
+        this.mbid = mbid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
